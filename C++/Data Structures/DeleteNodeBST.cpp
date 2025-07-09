208d89b7217f1b6ce5a4160c6d6d2b53a418cef2
Sayan Mondal <sayanmondal342@gmail.com>
2025-07-09 17:39:13 +0530

    Adding Delete Node BST
    Signed-off-by: Sayan Mondal <sayanmondal342@gmail.com>

---
+++ C++/Data Structures/DeleteNodeBST.cpp
@@ -0,0 +1,32 @@
+struct Node{
+	int data;
+	Node *left, *right;
+};
+
+Node* Delete(struct Node* root, int data) {
+    if(root == NULL) return root;
+    else if(data < root->data) root->left = Delete(root->left, data);
+    else if(data > root->data) root->right = Delete(root->right, data);
+    else{
+        if(root->left == NULL && root->right == NULL){
+            delete root;
+            root = NULL;
+        }
+        else if(root->left == NULL){
+            struct Node* temp = root;
+            root = root->right;
+            delete temp;
+        }
+        else if(root->right == NULL){
+            struct Node* temp = root;
+            root = root->left;
+            delete temp;
+        }
+        else {
+            struct Node* temp = FindMin(root->right);
+            root->data = temp->data;
+            root->right = Delete(root->right, temp->data);
+        }
+    }
+    return root;
+}
